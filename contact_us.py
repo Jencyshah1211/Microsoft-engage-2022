8309980e38186dba9cf08dd8deef25d59d51d8dd
Jencyshah1211 <102900102+Jencyshah1211@users.noreply.github.com>
2022-09-14 22:46:09 +0530

    Update contact_us.py

--- contact_us.py
+++ contact_us.py
@@ -14,20 +14,24 @@ def show_contact_page():
     st_lottie(lottie_contact)
     st.header(":mailbox: Get In Touch With Me!")
     contact_form = """
-    <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
-    <input type="hidden" name="_captcha" value="false">
-    <input type="text" name="name" placeholder="Your name" required>
-    <input type="email" name="email" placeholder="Your email" required>
-    <textarea name="message" placeholder="Your message here"></textarea>
-    <button type="submit">Send</button>
-    </form>
-    """
-    st.markdown(contact_form, unsafe_allow_html=True)
-    # Use Local CSS File
-    def local_css(file_name):
+<form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    local_css("style.css") 
+
+
+local_css("style.css")
 
 
 
